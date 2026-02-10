d1e3fa439a2f63a78ffba0e81e4a5159599b6fe7
sosena2 <149303452+sosena2@users.noreply.github.com>
2026-02-10 20:26:36 +0300

    feat: add routing structure and base pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import './App.css'
-import Footer from '../src/components/layout/Footer';
 
+import {Routes, Route} from 'react-router-dom';
+import Home from './pages/Home'
+import Products from './pages/Products';
+import ProductDetails from './pages/ProductDetails';
+import Cart from './pages/Cart';
+import NotFound from './pages/NotFound';
 function App() {
 
   return (
     <>
-      <Footer/>
+       <Routes>
+
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<ProductDetails />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="*" element={<NotFound />} />
+       </Routes>
+      
     </>
   )
 }
